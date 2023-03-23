2775d18f8f1c031907cf3fa6ed2b0720347ec396
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-03-22 20:35:05 -0500

    fix 2

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -98,7 +98,7 @@ Here is an example of the input:
 const sortByPrice = (arr) => {
   // Solution code here...
   return arr.sort((a, b) => a - b);
-}
+};
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
 
