7c71bbf68af9eb334cc364a4cdaa16e2f69d145b
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-05-30 23:17:49 -0500

    code 11

--- javascript/stack-and-queue/index.js
+++ javascript/stack-and-queue/index.js
@@ -28,4 +28,50 @@ class Stack {
   }
 }
 
-module.exports = { Stack };
+class PseudoQueue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+
+  enqueue(value) {
+    // Move all elements from stack1 to stack2
+    while (!this.stack1.isEmpty()) {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    // Push the new value to stack1
+    this.stack1.push(value);
+
+    // Move all elements back to stack1
+    while (!this.stack2.isEmpty()) {
+      this.stack1.push(this.stack2.pop());
+    }
+  }
+
+  dequeue() {
+    if (this.stack1.isEmpty()) {
+      throw new Error('PseudoQueue is empty');
+    }
+    return this.stack1.pop();
+  }
+  peek() {
+    if (this.stack1.isEmpty()) {
+      throw new Error('PseudoQueue is empty');
+    }
+    return this.stack1.peek();
+  }
+
+  isEmpty() {
+    return this.stack1.isEmpty();
+
+  }
+}
+
+
+
+module.exports = {
+  Stack,
+  PseudoQueue,
+};
